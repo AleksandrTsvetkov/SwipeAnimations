adecced475ce5e68eb5e01a6e98ec5b92ee09527
Alexander <aleksandrcw@gmail.com>
2020-06-02 23:47:32 +0300

    Initial Commit

---
+++ SwipeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeTest
+//
+//  Created by Александр Цветков on 02.06.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
