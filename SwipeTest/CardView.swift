cd3178596f3de4a0c0dea0375dbddf31bee1acbf
Alexander <aleksandrcw@gmail.com>
2020-06-05 17:59:17 +0300

    Add initial UI setup.

---
+++ SwipeTest/CardView.swift
@@ -0,0 +1,55 @@
+//
+//  CardView.swift
+//  SwipeTest
+//
+//  Created by Александр Цветков on 05.06.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    let shadowView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 10
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 5, height: 5)
+        view.layer.shadowRadius = 10
+        view.layer.shadowOpacity = 1
+        return view
+    }()
+    let imageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .clear
+        view.contentMode = .scaleToFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 10
+        addSubview(shadowView)
+        shadowView.addSubview(imageView)
+        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            shadowView.topAnchor.constraint(equalTo: self.topAnchor),
+            shadowView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            shadowView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            shadowView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: self.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SwipeTest/ViewController.swift
+++ SwipeTest/ViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let cardView: UIView = {
-        let view = UIView()
-        return view
-    }()
+    private let cardView = CardView()
+    private let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(cardViewDragged))
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        view.backgroundColor = .cyan
+        view.addSubview(cardView)
+        cardView.addGestureRecognizer(panGestureRecognizer)
+        cardView.imageView.image = UIImage(named: "pig")
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardView.heightAnchor.constraint(equalToConstant: view.frame.height / 2),
+            cardView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.75)
+        ])
+    }
+    
+    @objc private func cardViewDragged() {
+        
     }
     
 }
