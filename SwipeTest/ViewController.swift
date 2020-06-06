3a380687b09668ddf7838dba37c48f05864d750f
Alexander <aleksandrcw@gmail.com>
2020-06-07 00:08:43 +0300

    Add card movement and rotation.

--- SwipeTest/ViewController.swift
+++ SwipeTest/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let cardView = CardView()
-    private let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(cardViewDragged))
+    private var cardView = CardView()
+    private var panGestureRecognizer: UIPanGestureRecognizer!
+    private var dragToTop = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .cyan
         view.addSubview(cardView)
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(cardViewDragged))
         cardView.addGestureRecognizer(panGestureRecognizer)
         cardView.imageView.image = UIImage(named: "pig")
         cardView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,13 +31,35 @@ class ViewController: UIViewController {
             cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             cardView.heightAnchor.constraint(equalToConstant: view.frame.height / 2),
-            cardView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.75)
+            cardView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.9)
         ])
     }
     
     @objc private func cardViewDragged() {
-        
+        switch panGestureRecognizer.state {
+        case .began:
+            if panGestureRecognizer.location(in: cardView).y < cardView.frame.height / 2 {
+                dragToTop = true
+            } else {
+                dragToTop = false
+            }
+        case .changed:
+            let rotationPoint = 0.3 / view.frame.width * panGestureRecognizer.translation(in: cardView).x
+            let rotationWithDirection = dragToTop ? rotationPoint : -rotationPoint
+            print(rotationWithDirection)
+            cardView.transform = CGAffineTransform(rotationAngle: rotationWithDirection)
+            cardView.center.x = panGestureRecognizer.translation(in: cardView).x + view.center.x
+            cardView.center.y = panGestureRecognizer.translation(in: cardView).y + view.center.y
+        case .ended:
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, animations: {
+                self.cardView.center = self.view.center
+                self.cardView.transform = .identity
+            }) { _ in
+                
+            }
+            
+        default:
+            break
+        }
     }
-    
 }
-
